47c377fb02097f8a553ac4ccdb887cbc43d572c4
obtype <odonotme@protonmail.com>
2023-07-19 21:32:20 +0500

    Create displayController
    The disaplayController module manages the displaying of the
    game data. It also manages the inputs received from the screen
    from the user.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 
-const Player = function(){
-
+const Player = function(id, char){
+    
+    let name;
+    let score = 0;
+    
+    
+    const increaseScore = function() {
+        score++;
+        console.log("Current score of", this.name, "is", score);
+    }
+    
+    return{
+        name,
+        increaseScore,
+        char
+    }
 }
 
+player1 = Player(1, "X");
+player1.name = "apple";
+player2 = Player(2, "O");
+player2.name = "player 2: The Stick Meister ;}"
 
 const Game = (function() {
     let grid = [];
@@ -42,7 +60,8 @@ const Game = (function() {
         if((temp[0] && temp[1] && temp[2]) ||
          (temp[3] && temp[4] && temp[5]) ||
          (temp[6] && temp[7] && temp[8])){
-            console.log("Test Win! horisontal", playerChar);
+            console.log("Test Win! horisontal", playerChar);    //check if player has won horizontally
+            player1.char === playerChar ? player1.increaseScore() : player2.increaseScore(); 
             endGame();
             //console.log(grid);
         }
@@ -50,16 +69,31 @@ const Game = (function() {
         if((temp[0] && temp[3] && temp[6]) ||
          (temp[1] && temp[4] && temp[7]) ||
          (temp[2] && temp[5] && temp[8])){
-            console.log("Test Win! vertical", playerChar);
+            console.log("Test Win! vertical", playerChar);      //check if player has won vertically
+            player1.char === playerChar ? player1.increaseScore() : player2.increaseScore();
             endGame();
         }
 
         if((temp[0] && temp[4] && temp[8]) ||
          (temp[2] && temp[4] && temp[6])){
-            console.log("Test Win! diagonal", playerChar);
+            console.log("Test Win! diagonal", playerChar);      //check if player has won diagonally
+            player1.char === playerChar ? player1.increaseScore() : player2.increaseScore();
             endGame();
         }
 
+        let draw = true;
+
+        for(let i = 0; i < 9; i++){
+            if(!grid[i]){
+                draw = false;      
+                break;
+            }
+        }
+
+        if(draw){
+            console.log("The game is a draw.");
+            endGame();
+        }
     }
 
     const endGame = function () {
@@ -82,8 +116,61 @@ const Game = (function() {
 console.log(Game.grid);
 Game.playTurn(0);Game.playTurn(3);
 Game.playTurn(1);Game.playTurn(4);
-Game.playTurn(2);Game.playTurn(5);
+//Game.playTurn(2);Game.playTurn(5);
 
 console.log(Game.grid);
+
+//just finished the win conditions
+
+
+let displayController = function() {
+ 
+    let gridContainer = document.createElement("div");
+    let cellTemplate = document.createElement("div");
+    let cells = gridContainer.childNodes;
+
+
+    function createGrid() {
+        gridContainer.classList.add("grid");
+        cellTemplate.classList.add("cell");
+
+
+
+        for (let i = 0; i < 9; i++) {
+            let cell = cellTemplate.cloneNode("deep");
+            cell.setAttribute("index", i);
+
+            cell.addEventListener("click", (e) => {
+                Game.playTurn(e.target.getAttribute("index"));
+                updateGrid();
+            });
+
+
+            gridContainer.appendChild(cell);
+
+        }
+
+
+
+
+        document.querySelector("body").appendChild(gridContainer);
+
+    }
+    createGrid();
+
+    const updateGrid = function(){
+        for(let i = 0; i < 9; i++){
+            cells[i].textContent = Game.grid[i];
+        }
+    }
+
+
+    return{
+        updateGrid
+    }
+}();
+
+
+//displayController.updateGrid();
 
 
